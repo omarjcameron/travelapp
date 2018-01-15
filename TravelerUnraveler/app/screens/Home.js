e2a79340b8a48bbae084ea1f3178e82984c6f4b9
Omar Cameron <omarjcameron@gmail.com>
2018-01-15 13:06:05 -0800

    Add Reverse Currencies Functionality

--- TravelerUnraveler/app/screens/Home.js
+++ TravelerUnraveler/app/screens/Home.js
@@ -4,11 +4,14 @@ import { StatusBar } from 'react-native';
 import { Container } from '../components/Container';
 import { Logo } from '../components/Logo';
 import { InputWithButton } from '../components/TextInput';
+import { ClearButton } from '../components/Buttons';
 
 const TEMP_BASE_CURRENCY = 'USD';
-const TEMP_QUOTE_CURRENCY = "GBP";
+const TEMP_QUOTE_CURRENCY = 'GBP';
 const TEMP_BASE_PRICE = '100';
 const TEMP_QUOTE_PRICE = '79.74';
+const TEMP_CONVERSION_RATE = 0.7974;
+
 
 
 class Home extends Component {
@@ -22,6 +25,10 @@ class Home extends Component {
 
   handleTextChange = (text) => {
     console.lgo('change text', text);
+  };
+
+  handleSwapCurrency = () => {
+    console.log('press swap currency');
   };
 
   render() {
@@ -41,6 +48,10 @@ class Home extends Component {
           buttonText={TEMP_QUOTE_CURRENCY}
           editable={false}
           value={TEMP_QUOTE_PRICE}
+        />
+        <ClearButton
+          text="Reverse Currencies"
+          onPress={this.handleSwapCurrency}
         />
       </Container>
     );
