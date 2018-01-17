09ddf2753be09c7428dcc5d610b181fa10566601
Omar Cameron <omarjcameron@gmail.com>
2018-01-17 10:13:39 -0800

    Fix reducers to handle actions of initial state and conversion results and errors

--- TravelerUnraveler/app/config/sagas.js
+++ TravelerUnraveler/app/config/sagas.js
@@ -1,12 +1,25 @@
-import { takeEvery } from 'redux-saga/effects';
-import { SWAP_CURRENCY, CHANGE_BASE_CURRENCY, GET_INITIAL_CONVERSION } from '../actions/currencies';
+import { takeEvery, select, call, put } from 'redux-saga/effects';
+import { SWAP_CURRENCY, CHANGE_BASE_CURRENCY, GET_INITIAL_CONVERSION, CONVERSION_RESULT, CONVERSION_ERROR } from '../actions/currencies';
 
 const getLatestRate = currency => fetch(`http://api.fixer.io/latest?base=${currency}`);
+
 function* fetchLatestConversionRates(action) {
-  console.log('TODO: Update the stuff', action);
-  getLatestRate('USD')
-    .then(res => res.json()).then(res => console(res));
-  yield;
+  try {
+    let currency = action.currency;
+    if (currency === undefined) {
+      currency = yield select(state => state.currencies.baseCurrency);
+    }
+    const response = yield call(getLatestRate, currency);
+    const result = yield response.json();
+
+    if (result.error) {
+      yield put({ type: CONVERSION_ERROR, error: result.error });
+    } else {
+      yield put({ type: CONVERSION_RESULT, result });
+    }
+  } catch (e) {
+    yield put({ type: CONVERSION_ERROR, error: e.message });
+  }
 }
 
 export default function* rootSaga() {
--- TravelerUnraveler/app/reducers/currencies.js
+++ TravelerUnraveler/app/reducers/currencies.js
@@ -1,10 +1,19 @@
-import { CHANGE_CURRENCY_AMOUNT, SWAP_CURRENCY, CHANGE_BASE_CURRENCY, CHANGE_QUOTE_CURRENCY } from '../actions/currencies';
+import {
+  CHANGE_CURRENCY_AMOUNT,
+  SWAP_CURRENCY,
+  CHANGE_BASE_CURRENCY,
+  CHANGE_QUOTE_CURRENCY,
+  GET_INITIAL_CONVERSION,
+  CONVERSION_RESULT,
+  CONVERSION_ERROR,
+} from '../actions/currencies';
 
 const initialState = {
   baseCurrency: 'USD',
   quoteCurrency: 'GBP',
   amount: 100,
   conversions: {},
+  error: null,
 };
 
 const setConversions = (state, action) => {
@@ -48,6 +57,28 @@ export default (state = initialState, action) => {
         ...state,
         quoteCurrency: action.currency,
         conversions: setConversions(state, action),
+      };
+    case GET_INITIAL_CONVERSION:
+      return {
+        ...state,
+        conversions: setConversions(state, { currency: state.baseCurrency }),
+      };
+    case CONVERSION_RESULT:
+      return {
+        ...state,
+        baseCurrency: action.result.base,
+        conversions: {
+          ...state.conversions,
+          [action.result.base]: {
+            isFetching: false,
+            ...action.result,
+          },
+        },
+      };
+    case CONVERSION_ERROR:
+      return {
+        ...state,
+        error: action.error,
       };
     default:
       return state;
